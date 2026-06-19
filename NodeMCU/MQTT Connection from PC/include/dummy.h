d0a24959931f3c39315aa40407a76f4d044a67bd
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-06-19 16:04:20 +0200

    Add dummy.h header file for MQTT connection

---
+++ NodeMCU/MQTT Connection from PC/include/dummy.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MQTT_SERVER   "examplehivemqlink.com"
+#define MQTT_PORT     8883
+#define MQTT_USER     "user"
+#define MQTT_PASSWORD "password"
+#define WIFI_SSID     "Wokwi-GUEST"
+#define WIFI_PASS     ""
