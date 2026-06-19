32891d6e67c8db50fa90ab56e9cc0f40a83902b5
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-06-19 15:59:44 +0200

    chore: refined project to one folder

---
+++ NodeMCU/MQTT Connection from PC/include/env.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MQTT_SERVER   "dc8a6f487f8345a28185940816d843d4.s1.eu.hivemq.cloud"
+#define MQTT_PORT     8883
+#define MQTT_USER     "connector"
+#define MQTT_PASSWORD "moseJacopo1"
+#define WIFI_SSID     "Wokwi-GUEST"
+#define WIFI_PASS     ""
